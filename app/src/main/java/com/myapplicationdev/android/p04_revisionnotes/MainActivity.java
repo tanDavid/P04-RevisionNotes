db5f0b584aaf452c14bbf56fce0538acd04d3061
19007457 <19007457@myrp.edu.sg>
2021-05-04 14:30:35 +0800

    Julien's MainActivity.java commit

--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/MainActivity.java
@@ -41,8 +41,8 @@ public class MainActivity extends AppCompatActivity {
         btnShow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(intent);
+                Intent n = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(n);
             }
         });
 
